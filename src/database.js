2ff31f50a41e2fd48e7e637ff1e04dd14c3b431e
loweffort-alt <farfan_alexander@outlook.com>
2023-09-08 10:10:39 -0500

    Fix: config.js and database.js indentation was wrong

--- src/database.js
+++ src/database.js
@@ -2,11 +2,11 @@ import { Pool } from "pg";
 import { db } from "./config";
 
 const pool = new Pool({
-	user: db.user,
-	password: db.password,
-	host: db.host,
-	port: db.port,
-	database: db.database
+  user: db.user,
+  password: db.password,
+  host: db.host,
+  port: db.port,
+  database: db.database,
 });
 
 export default pool;
