2d953b31e4a2c0cbffabd45d5437afc20e2b0ff4
loweffort-alt <farfan_alexander@outlook.com>
2023-09-08 11:47:36 -0500

    Conexion database DONE

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
-import { config } from "dotenv";
-config();
+//import { config } from "dotenv";
+//config();
 
-export const db = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE,
-};
+//export const db = {
+//user: process.env.DB_USER,
+//password: process.env.DB_PASSWORD,
+//host: process.env.DB_HOST,
+//port: process.env.DB_PORT,
+//database: process.env.DB_DATABASE,
+//};
--- src/database.js
+++ src/database.js
@@ -1,12 +1,15 @@
-import pg from "pg";
-import { db } from "./config.js";
+import { Pool } from "pg";
+import { config } from "dotenv";
+config();
 
-const pool = new pg.Pool({
-  user: db.user,
-  password: db.password,
-  host: db.host,
-  port: db.port,
-  database: db.database,
+const pool = new Pool({
+  //user: db.user,
+  //password: db.password,
+  //host: db.host,
+  //port: db.port,
+  //database: db.database
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
 });
 
 export default pool;
